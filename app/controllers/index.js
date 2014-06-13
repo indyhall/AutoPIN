6f840db1cabe8d8d16e8538b9d71863b765df78f
Chris Morrell <github@rchy.net>
2014-06-13 12:43:59 -0400

    Simple TiBeacons test app

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,30 @@
-function doClick(e) {
-    alert($.label.text);
-}
-
 $.index.open();
+
+var TiBeacons = require('org.beuckman.tibeacons');
+
+TiBeacons.enableAutoRanging();
+
+TiBeacons.startMonitoringForRegion({
+    uuid : "D57092AC-DFAA-446C-8EF3-C81AA22815B5",
+    identifier : "Chris's Mac",
+    major: 5,
+    minor: 5000
+});
+
+TiBeacons.addEventListener("enteredRegion", function(e) {
+	Ti.API.info('enteredRegion: ' + JSON.stringify(e, null, 4));
+	alert('Entered region.');
+});
+
+TiBeacons.addEventListener("exitedRegion", function(e) {
+	Ti.API.info('exitedRegion: ' + JSON.stringify(e, null, 4));
+});
+
+TiBeacons.addEventListener("determinedRegionState", function(e) {
+	Ti.API.info('determinedRegionState: ' + JSON.stringify(e, null, 4));
+});
+
+TiBeacons.addEventListener("beaconProximity", function(e){
+   $.proximity.setText('Range: ' + e.proximity);
+   Ti.API.info('beaconProximity: ' + JSON.stringify(e, null, 4));
+});
